71dfbde6e158fa5df42f5ab2cf78c2ec0435c207
code-zebra-java <73904245+code-zebra-java@users.noreply.github.com>
2025-02-08 00:45:51 -0500

    Completed second initial for question 1

--- Assignment1.java
+++ Assignment1.java
@@ -4,22 +4,22 @@ public class Assignment1 {
   public static void main(String[] args) {
 
     //Question 1: print your initials in bold letters
-    System.out.println(" T T T T T T T T T T T T ");
+    System.out.println(" T T T T T T T T T T T T " + "\t    S S S S S S S S");
 
-    System.out.println(" T T T T T T T T T T T T ");
+    System.out.println(" T T T T T T T T T T T T " + "\t  S S S S S S S S S S");
 
-    System.out.println(" T T T T T T T T T T T T ");
+    System.out.println(" T T T T T T T T T T T T " + "\t  S S S S   \t S S S");
 
-    System.out.println("\t T T T T \t");
+    System.out.println("\t T T T T \t" + "\t   S S S S S S ");
 
-    System.out.println("\t T T T T \t");
+    System.out.println("\t T T T T \t" + "\t      S S S S S S ");
 
-    System.out.println("\t T T T T \t");
+    System.out.println("\t T T T T \t" + "\t\t S S S S S S ");
 
-    System.out.println("\t T T T T \t");
+    System.out.println("\t T T T T \t" + "\t S S S \t      S S S S");
 
-    System.out.println("\t T T T T \t");
+    System.out.println("\t T T T T \t" + "\t  S S S S S S S S S S");
 
-    System.out.println("\t T T T T \t");
+    System.out.println("\t T T T T \t" + "\t    S S S S S S S S");
   }
 }
