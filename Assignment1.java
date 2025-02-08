f9f25a0d8141eb0b92513204518cac5693e15e8a
code-zebra-java <73904245+code-zebra-java@users.noreply.github.com>
2025-02-08 00:04:57 -0500

    Programmed the first initial for question 1

--- Assignment1.java
+++ Assignment1.java
@@ -2,6 +2,24 @@
 
 public class Assignment1 {
   public static void main(String[] args) {
-    // Add your code here
+
+    //Question 1: print your initials in bold letters
+    System.out.println(" T T T T T T T T T T T T ");
+
+    System.out.println(" T T T T T T T T T T T T ");
+
+    System.out.println(" T T T T T T T T T T T T ");
+
+    System.out.println("\t T T T T \t");
+
+    System.out.println("\t T T T T \t");
+
+    System.out.println("\t T T T T \t");
+
+    System.out.println("\t T T T T \t");
+
+    System.out.println("\t T T T T \t");
+
+    System.out.println("\t T T T T \t");
   }
 }
