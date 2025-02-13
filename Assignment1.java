1c05e43dc56b2bde49b8a1cd7a503f060c7d2f3e
code-zebra-java <73904245+code-zebra-java@users.noreply.github.com>
2025-02-13 02:44:10 -0500

    Completed writing the program for question 2

--- Assignment1.java
+++ Assignment1.java
@@ -1,4 +1,5 @@
 // Import libraries here
+import java.util.Scanner;
 
 public class Assignment1 {
   public static void main(String[] args) {
@@ -21,5 +22,17 @@ public class Assignment1 {
     System.out.println("\t T T T T \t" + "\t  S S S S S S S S S S");
 
     System.out.println("\t T T T T \t" + "\t    S S S S S S S S");
+
+    //Question 2: Ask user for number in fahrenheit and convert the number to Celsius
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Please enter a number in Fahrenheit: ");
+
+    double fahrenheit = scan.nextDouble();
+
+    double celsius = (fahrenheit-32)*(5/9);
+
+    scan.close();
+
   }
 }
